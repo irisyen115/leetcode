6cf59d643c0ca6552f0aefd0cdb2d128ee9fc268
iris <110B16890@mailst.cjcu.edu.tw>
2022-05-26 19:34:28 +0800

    write 1137 1365 2235 509

---
+++ java/1137.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int tribonacci(int n) {
+        int []answer = new int[38];
+        answer[0] = 0;
+        answer[1] = 1;
+        answer[2] = 1;
+        
+        for(int i = 3; i < answer.length; i++){
+            answer[i] = answer[i-1] + answer[i-2] + answer[i-3];
+        }
+        
+        return answer[n];
+    }
+}
---
+++ java/509.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int fib(int n) {
+        int []F = new int[31];
+        F[0] = 0;
+        F[1] = 1;
+        
+        for(int i = 2; i < F.length; i++) {
+            F[i] = F[i-1] + F[i-2];
+        }
+        return F[n];
+    }
+}
