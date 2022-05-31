7316773afc6addce2c3c638c0f5f23db52907e64
iris <110B16890@mailst.cjcu.edu.tw>
2022-05-31 17:46:17 +0800

    change method

--- java/344.java
+++ java/344.java
@@ -1,10 +1,13 @@
 class Solution {
     public char[] reverseString(char[] s) {
-        for (int i = 0 ; i < s.length/2 ; i++) {
-				char tmp = s[s.length-1-i];
-				s[s.length-1-i] = s[i];
-				s[i]= tmp;
-		}
+        int left = 0, right = s.length - 1;
+        while(left < right) {
+            char tmp = s[right];
+            s[right] = s[left];
+            s[left] = tmp;
+            left++;
+            right--;
+        } 
 		return s;
     }
-}
+}}
