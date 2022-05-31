a5a9d2d0763f915e07bb9aa5c6a660acb9a23e75
iris <110B16890@mailst.cjcu.edu.tw>
2022-05-31 17:56:02 +0800

    change int to boolean

--- java/2103.java
+++ java/2103.java
@@ -1,23 +1,29 @@
 class Solution {
     public int countPoints(String rings) {
         char[] chars = rings.toCharArray();
-        int[][] ans = new int[10][3];
+        boolean[][] ans = new boolean[10][3];
         int count = 0;
+        
+        for(boolean[] row:ans){
+            for(boolean j:row){
+                j = false;
+            }
+        }
         
         for(int i = 1; i < chars.length; i+=2){
             int row = chars[i] - '0';
             if(chars[i-1] == 'B'){
-                ans[row][0] += 1;
+                ans[row][0] = true;
             }
             if(chars[i-1] == 'G'){
-                ans[row][1] += 1;
+                ans[row][1] = true;
             } 
             if(chars[i-1] == 'R'){
-                ans[row][2] += 1;
+                ans[row][2] = true;
             }
         }
         for(int i = 0; i < ans.length; i++){
-            if(ans[i][0] > 0 && ans[i][1] > 0 && ans[i][2] > 0){ 
+            if(ans[i][0] == true && ans[i][1] == true && ans[i][2] == true){ 
                 count++;
             }
         }
