439f92c8719807c7f50f27a19ccd9d069f60bcad
iris <110b16890@mailst.cjcu.edu.tw>
2022-07-07 17:56:20 +0800

    Sum of All Odd Length Subarrays

--- java/303.java
+++ java/303.java
@@ -1,8 +1,16 @@
-class Solution {
-    public int[] runningSum(int[] nums) {
-        for(int i = 1; i < nums.length; i++) {
-            nums[i] = nums[i] + nums[i-1];
+class NumArray {
+
+    public NumArray(int[] nums) {
+        p = new int[nums.length + 1];
+        p[0] = 0;
+        for(int i = 1; i < p.length; i++) {
+            p[i] = p[i-1] + nums[i-1];
         }
-        return nums;
     }
+    
+    public int sumRange(int left, int right) {
+        return p[right+1] - p[left];
+    }
+    
+    private int[] p;
 }
