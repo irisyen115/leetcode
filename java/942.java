24035381afe9da009e051fc72dc771526b9ed2f8
iris yen <110b16890@mailst.cjcu.edu.tw>
2022-07-15 20:36:18 +0800

    revise DI String Match

--- java/942.java
+++ java/942.java
@@ -1,23 +1,15 @@
 class Solution {
     public int[] diStringMatch(String s) {
         int[] ans = new int[s.length()+1];
-        char[] a = s.toCharArray();
-        int I = 0;
-        int D = a.length;
-        for(int i = 0; i < a.length; i++) {
-            if(a[i] == 'I') {
-                ans[i] = I;
-                I++;
+        int I = 0, D = ans.length-1;
+        for(int i = 0; i < ans.length-1; i++) {
+            if(s.charAt(i) == 'I') {
+                ans[i] = I++;
             } else {
-                ans[i] = D;
-                D--;
+                ans[i] = D--;
             }
-        }
-        if(a[a.length-1] == 'I') {
-                ans[ans.length-1] = I;                
-            } else {
-                ans[ans.length-1] = D;                
-            }
+        }    
+        ans[ans.length-1] = I;                            
         return ans; 
     }
 }
