231d1f77cec494977ae368937f91e7cfedbc98df
iris yen <110b16890@mailst.cjcu.edu.tw>
2022-07-19 17:55:06 +0800

    Build an Array With Stack Operations

--- java/1441.java
+++ java/1441.java
@@ -1,23 +1,17 @@
 class Solution {
     public List<String> buildArray(int[] target, int n) {
-        List<String> myList = new ArrayList<String>();
-        int index = 1;
-        while(index < target[target.length-1] + 1) {
-            myList.add("Push");
-            if(contains(target,index) == false) {
-                myList.add("Pop");
+        List<String> a = new ArrayList<String>();
+        int index = 0;
+        int pos = 1;
+        while(index < target.length) {
+            a.add("Push");
+            if(pos != target[index]) {
+                a.add("Pop");
+            } else {
+                index++;
             }
-            index++;
+            pos++;
         }
-        return myList;
-    }
-    public static boolean contains(final int[] arr, final int key) {
-        boolean found = false;
-        for(int i = 0; i < arr.length; i++) {
-            if(arr[i] == key) {
-                found = true;
-            }
-        }
-        return found; 
+        return a;
     }
 }
