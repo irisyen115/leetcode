8f04c044af0bdfa3e9167145b97d1c81e55a5cc8
iris yen <110b16890@mailst.cjcu.edu.tw>
2022-08-01 19:45:06 +0800

    Binary Tree Postorder Traversal

---
+++ java/145.java
@@ -0,0 +1,13 @@
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> ans = new ArrayList<Integer>();
+        
+        if(root == null) return ans;
+        
+        ans.addAll(postorderTraversal(root.left));
+        ans.addAll(postorderTraversal(root.right));
+        ans.add(root.val);
+        
+        return ans;
+    }
+}
