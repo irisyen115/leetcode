ba75390388c1dd876293a2a7ba024b0225835d62
iris yen <110b16890@mailst.cjcu.edu.tw>
2022-08-15 19:05:47 +0800

    Maximum Product Difference Between Two Pairs

--- java/1913.java
+++ java/1913.java
@@ -1,6 +1,27 @@
 class Solution {
     public int maxProductDifference(int[] nums) {
-        Arrays.sort(nums);
-        return (nums[nums.length-1] * nums[nums.length-2]) - (nums[0] * nums[1]);
+        int max1 = -1;
+        int max2 = -1;
+        int min1 = 100000;
+        int min2 = 100000;
+        for(int num:nums) {
+            if(num > max2) {
+                max2 = num;
+            }
+            if(num > max1) {
+                if(num < max2) continue;
+                max2 = max1;
+                max1 = num;                
+            }
+            if(num < min2) {
+                min2 = num;
+            }
+            if(num < min1) {
+                if(num > min2) continue;
+                min2 = min1;
+                min1 = num;                
+            }
+        }        
+        return (max1 * max2) - (min1 * min2);
     }
 }
