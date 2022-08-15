70ffff5cd48d2bd7c0d777e4a225b02da4be2861
iris yen <110b16890@mailst.cjcu.edu.tw>
2022-08-15 18:02:34 +0800

    Maximum Product Difference Between Two Pairs

--- java/206.java
+++ java/206.java
@@ -12,16 +12,3 @@ class Solution {
         return prev;
     }
 }
-c語言
-struct ListNode* reverseList(struct ListNode* head){
-    struct ListNode * prev = NULL;
-    struct ListNode * cur = head;
-    struct ListNode * tmp;
-    while(cur != NULL) {
-        tmp = cur->next;
-        cur->next = prev;
-        prev = cur;
-        cur = tmp;
-    }
-    return prev;
-}
