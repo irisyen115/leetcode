d89cbe14738cbe7ba5737c1108cc3fd2bdf549e8
iris yen <110b16890@mailst.cjcu.edu.tw>
2022-08-20 22:09:56 +0800

    Flipping an Image

--- java/832.java
+++ java/832.java
@@ -8,19 +8,27 @@ class Solution {
                 tmp = nums[head];
                 nums[head] = nums[tail];
                 nums[tail] = tmp;
+                if(nums[head] == 0) {
+                    nums[head] = 1;
+                } else {
+                    nums[head] = 0;
+                }
+                if(nums[tail] == 0) {
+                    nums[tail] = 1;
+                } else {
+                    nums[tail] = 0;
+                }                
                 head++;
                 tail--;
+                }  
+            if(nums.length % 2 == 1 && nums[nums.length/2] == 0) {
+                nums[nums.length/2] = 1;
+            } else if(nums.length % 2 == 1 && nums[nums.length/2] == 1){
+                nums[nums.length/2] = 0;
             }
-            for(int i = 0; i < nums.length; i++) {
-                if(nums[i] == 0) {
-                    nums[i] = 1;
-                } else {
-                    nums[i] = 0;
-                }
-            }                        
-        }
+            }    
         return image;
-    }
+        }
 }
-    
+
 
