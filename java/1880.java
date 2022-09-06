e755e87e3c032e581fd33477f8b230cd1eb31af5
irisyen115 <110b16890@mailst.cjcu.edu.tw>
2022-09-06 11:54:11 +0800

    Check if Word Equals Summation of Two Words

--- java/1880.java
+++ java/1880.java
@@ -1,22 +1,22 @@
 class Solution {
-    public boolean isSumEqual(String firstWord, String secondWord, String targetWord) {
-        char[] a = firstWord.toCharArray();
-        char[] b = secondWord.toCharArray();
-        char[] c = targetWord.toCharArray();        
-        int ac = 0;
-        int bc = 0;
-        int cc = 0;
+  public boolean isSumEqual(String firstWord, String secondWord, String targetWord) {
+      char[] a = firstWord.toCharArray();
+      char[] b = secondWord.toCharArray();
+      char[] c = targetWord.toCharArray();        
+      int ac = 0;
+      int bc = 0;
+      int cc = 0;
 
-        for(int i = 0; i < a.length; i++) {
-          ac += ((int)(a[i]-'a') * (Math.pow(10 , a.length-1-i)));
-        }
+      for(int i = 0; i < a.length; i++) {
+          ac = ac * 10 +(int)(a[i]-'a');
+      }
 
-        for(int i = 0; i < b.length; i++) {
-          bc += ((int)(b[i]-'a') * (Math.pow(10 , b.length-1-i)));
-        }
-        for(int i = 0; i < c.length; i++) {
-          cc += ((int)(c[i]-'a') * (Math.pow(10 , c.length-1-i)));
-        }
-        return ac + bc == cc;
-    }
+      for(int i = 0; i < b.length; i++) {
+          bc = bc * 10 +(int)(b[i]-'a');
+      }
+      for(int i = 0; i < c.length; i++) {
+          cc = cc * 10 +(int)(c[i]-'a');
+      }
+      return ac + bc == cc;
+  }
 }
