7bda82e4199e69d26026277f5607c49eec41a913
irisyen115 <98208602+irisyen115@users.noreply.github.com>
2022-11-26 23:14:43 +0800

    w47 total 2/10 242. Valid Anagram

---
+++ py/242.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        a = Counter(s)
+        b = Counter(t)
+        return a == b
