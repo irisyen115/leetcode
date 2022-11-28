0893d33f76f75bdaefffe4384c3a161ec20e61e7
irisyen115 <110b16890@mailst.cjcu.edu.tw>
2022-11-28 22:53:29 +0800

    w47 total 7/10 144. Binary Tree Preorder Traversal 94. Binary Tree Inorder Traversal 145. Binary Tree Postorder Traversal

--- py/884.py
+++ py/884.py
@@ -5,11 +5,4 @@ class Solution(object):
         :type s2: str
         :rtype: List[str]
         """
-        a = Counter(s1.split())
-        b = Counter(s2.split())
-        c = set(s1.split()) ^ set(s2.split())
-        ans = []
-        for v in c:
-            if a[v] == 1 or b[v] == 1:
-                ans.append(v)
-        return ans
+        return [k for k, v in Counter(s1.split() + s2.split()).items() if v == 1]
