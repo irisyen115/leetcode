b6f79afe9a6263ff4b65d627309c92e931358b12
irisyen115 <110b16890@mailst.cjcu.edu.tw>
2023-03-04 13:49:14 +0800

    w9 total 4/6 383. Ransom Note

--- py/1869.py
+++ py/1869.py
@@ -3,28 +3,8 @@ class Solution(object):
         """
         :type s: str
         :rtype: bool
-        """                        
-        M0 = -1
-        M1 = -1
-        count0 = 0
-        count1 = 0
-        for i in range(len(s)):
-            if i < len(s) - 1:
-                if s[i] == '0':
-                    count0 += 1
-                    M0 = max(M0, count0)
-                    if s[i+1] == '1':
-                        count0 = 0
-                elif s[i] == '1':
-                    count1 += 1
-                    M1 = max(M1, count1)
-                    if s[i+1] == '0':
-                        count1 = 0                        
-            else:
-                if s[i] == '0':
-                    count0 += 1
-                    M0 = max(M0, count0)
-                elif s[i] == '1':
-                    count1 += 1
-                    M1 = max(M1, count1)
-        return M1 > M0
+        """      
+        d = {'0': 0, '1': 0}
+        for x, y in itertools.groupby(s):
+            d[x] = max(d[x], sum(1 for _ in y))
+        return d['1'] > d['0']
