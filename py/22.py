95a9c9ed7a29577595684f38a3a02964a0739ed5
irisyen115 <110b16890@mailsr.cjcu.edu.tw>
2023-07-02 11:57:36 +0800

    w27 total 2/2 1991. Find the Middle Index in Array 22. Generate Parentheses

---
+++ py/22.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def generateParenthesis(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        def dfs(left, right, s):
+            if len(s) == n * 2:
+                a.append(s)
+                return 
+
+            if left < n:
+                dfs(left + 1, right, s + '(')
+
+            if right < left:
+                dfs(left, right + 1, s + ')')
+
+        a = []
+        dfs(0, 0, '')
+        return a
