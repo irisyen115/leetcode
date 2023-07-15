9c2d850936892557b9c01d369e0e0ec1061a0dc3
irisyen115 <110b16890@mailsr.cjcu.edu.tw>
2023-07-15 16:44:54 +0800

    w28 total 14/12 219. Contains Duplicate II

---
+++ py/219.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def containsNearbyDuplicate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: bool
+        """        
+        d = defaultdict(list)
+        for i in range(len(nums)):
+            d[nums[i]].append(i)
+
+        for key, value in d.items():
+            if len(value) > 1:
+                for a,b in zip(value, value[1:]):
+                    if b - a <= k:
+                        return True
+        return False
