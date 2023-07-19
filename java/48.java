7358aff7d20ae680bd4a0cfcffc57460ab5fc948
irisyen115 <110b16890@mailsr.cjcu.edu.tw>
2023-07-19 19:28:38 +0800

    correct

--- java/48.java
+++ java/48.java
@@ -1,26 +1,14 @@
 class Solution {
     public void rotate(int[][] matrix) {
-        int head = 0;
-        int tail = matrix.length - 1;
-
-        while (head < tail) {
-            for (int i = 0; i < matrix.length; i++){
-                int tmp;
-                tmp = matrix[head][i];
-                matrix[head][i] = matrix[tail][i];
-                matrix[tail][i] = tmp;
+        int n = matrix.length - 1;
+        for (int i = 0; i < matrix.length / 2; i++) {
+            for (int j = i; j < n - i; j++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[n - j][i];
+                matrix[n - j][i] = matrix[n - i][n - j];
+                matrix[n - i][n - j] = matrix[j][n - i];
+                matrix[j][n - i] = tmp;
             }
-            head ++;
-            tail --;
         }
-
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = i + 1; j < matrix.length; j++) {
-                int tmp;
-                tmp = matrix[i][j];
-                matrix[i][j] = matrix[j][i];
-                matrix[j][i] = tmp;
-            }
-        }        
     }
 }
