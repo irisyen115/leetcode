c5f1591e9f6e5cee3ed7cad463f9ff2277a365ba
irisyen115 <110b16890@mailsr.cjcu.edu.tw>
2023-08-18 14:35:50 +0800

    w33 total 13/12 2798. Number of Employees Who Met the Target

---
+++ py/2798.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def numberOfEmployeesWhoMetTarget(self, hours, target):
+        """
+        :type hours: List[int]
+        :type target: int
+        :rtype: int
+        """
+        return sum(hour >= target for hour in hours)
