fe92fef17fa43d871e2b6dc11126d6a6f505a0c4
irisyen115 <110b16890@mailst.cjcu.edu.tw>
2023-12-13 22:15:46 +0800

    w50 2/10 867. Transpose Matrix 2706. Buy Two Chocolates

---
+++ py/2942.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def findWordsContaining(self, words, x):
+        """
+        :type words: List[str]
+        :type x: str
+        :rtype: List[int]
+        """
+        return [i for i in range(len(words)) if x in words[i]]
